d8caa85bf2a81b9e362c2f9ab234dcf0ad494226
Jeferson1313 <jefersonsantanasantos13@gmail.com>
2024-12-03 09:01:36 -0300

    03-12

--- Request.py
+++ Request.py
@@ -30,16 +30,14 @@ base_url = 'https://apix.toqweb.com.br:2004/rel/maxus/kpi/'
 
 
 urls = [
-    'VendasBiEmpresa',
-    'VendasBiOPV',
+    'VendasEmpresa',
+    'Vendas',
     'BalanceteBiMes',
-    'VendasBiProduto',
+    'Produto',
     'TitulosPagos'
 ]
 
 for url_final in urls:
-
-    url = base_url+url_final
 
     tokenRetorno = pegartoken.token
 
@@ -48,45 +46,110 @@ for url_final in urls:
             'xformat' : 'csv'
         }
 
-    #todos os endpoints usados na consulta estão usando a mesma classeDTO para não dar erro e poder passar todos os campos previamente no body
-    body = {
-            'TipoConsulta' : '', 
-            'Ordem' : '',
-            'Detalhado' : False,
-            'DataAlt' : dataConsultaAnt,
-            'DataIni' : dataconsultaAntIniStr,
-            'DataFim' : dataConsultaAnt,
-        }
+    if url_final == 'Vendas':
+        tipoConsulta = [
+            '-',
+            'vendedor',
+            'pessoaempresa',
+            'condpagamento',
+            'meiopagamento',
+            'caixapdv'
+        ]
 
-    api_response = requests.post(url, json=body, headers=headers)
+    if url_final == 'Produto':
+        tipoConsulta = [
+            'produtoempresa',
+            'secao',
+            'grupo',
+            'categoria',
+            'subcategoria',
+            'setor',
+            'impostoicms'
+        ]
 
-    if api_response.status_code == 200:
-            
-            print("Solicitação bem-sucedida!")
-            set_env_variable('DATACONSULTA', dataconsulta)
+    if url_final == 'Vendas' or url_final == 'Produto':
+        for consulta in tipoConsulta:
+            url = base_url+url_final
+            fileName = url_final+consulta
+            body = {
+                    'TipoConsulta' : consulta, 
+                    'Ordem' : '',
+                    'Detalhado' : False,
+                    'DataAlt' : dataConsultaAnt,
+                    'DataIni' : dataconsultaAntIniStr,
+                    'DataFim' : dataConsultaAnt,
+                }
 
-            file = url_final +".csv"
-        
-            try:
-                csv_data = api_response.content.decode('latin-1') 
+            api_response = requests.post(url, json=body, headers=headers)
+
+            if api_response.status_code == 200:
+                    
+                    print("Solicitação bem-sucedida!")
+                    set_env_variable('DATACONSULTA', dataconsulta)
+
+                    file = fileName +".csv"
                 
-                df = pd.read_csv(
-                    io.StringIO(csv_data), 
-                    sep=';', 
-                    decimal=',',  
-                    engine='python' 
-                )
-                print(df.head())  
+                    try:
+                        csv_data = api_response.content.decode('latin-1') 
+                        
+                        df = pd.read_csv(
+                            io.StringIO(csv_data), 
+                            sep=';', 
+                            decimal=',',  
+                            engine='python' 
+                        )
+                        print(df.head())  
+                        
+                        df.to_csv(file, sep=';', decimal=',', index=False, encoding='utf-8-sig')
+                        print(f"Dados salvos em {file} no formato adequado para Excel!")
                 
-                df.to_csv(file, sep=';', decimal=',', index=False, encoding='utf-8-sig')
-                print(f"Dados salvos em {file} no formato adequado para Excel!")
-        
-            except Exception as e:
-                print(f"Erro ao processar os dados CSV: {e}")
-                print("Conteúdo recebido:")
-                print(api_response.text[:500]) 
+                    except Exception as e:
+                        print(f"Erro ao processar os dados CSV: {e}")
+                        print("Conteúdo recebido:")
+                        print(api_response.text[:500]) 
+            else:
+                print(f"Falha ao acessar a API: {api_response.status_code}")
+                print("Resposta:", api_response.text)
+
     else:
-        print(f"Falha ao acessar a API: {api_response.status_code}")
-        print("Resposta:", api_response.text)
+        url = base_url+url_final
+        body = {
+                    'Ordem' : '',
+                    'Detalhado' : False,
+                    'DataAlt' : dataConsultaAnt,
+                    'DataIni' : dataconsultaAntIniStr,
+                    'DataFim' : dataConsultaAnt,
+                }
+
+        api_response = requests.post(url, json=body, headers=headers)
+
+        if api_response.status_code == 200:
+                    
+                print("Solicitação bem-sucedida!")
+                set_env_variable('DATACONSULTA', dataconsulta)
+
+                file = url_final +".csv"
+                
+                try:
+                    csv_data = api_response.content.decode('latin-1') 
+                        
+                    df = pd.read_csv(
+                        io.StringIO(csv_data), 
+                        sep=';', 
+                        decimal=',',  
+                        engine='python' 
+                    )
+                    print(df.head())  
+                        
+                    df.to_csv(file, sep=';', decimal=',', index=False, encoding='utf-8-sig')
+                    print(f"Dados salvos em {file} no formato adequado para Excel!")
+                
+                except Exception as e:
+                    print(f"Erro ao processar os dados CSV: {e}")
+                    print("Conteúdo recebido:")
+                    print(api_response.text[:500]) 
+        else:
+            print(f"Falha ao acessar a API: {api_response.status_code}")
+            print("Resposta:", api_response.text)
 
 print('Consulta Finalizada!')
